e8c6be60fa952179ebb3f44b064fe611bbd795d5
akshay <akshaysjk@gmail.com>
2018-03-19 14:47:28 -0400

    updating location file

--- Algos_Project/src/project/Location.java
+++ Algos_Project/src/project/Location.java
@@ -1,17 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package project;
 
 /**
  *
- * @author ayush
+ * @author akshay
  */
 public class Location {
-    
-    
+
+
     private double[] loc;
 
 	public Location(double[] loc) {
